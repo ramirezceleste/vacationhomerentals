2842f39f30b433714aa68fe0fe486a28d5ffff58
Johana Ramirez <JCRamirez@Johanas-MacBook-Pro.local>
2018-06-23 12:47:23 -0700

    added new files for apartments

---
+++ assets/javascript/apartments.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
+    });
+  });
+
+  console.log("working");
+
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
